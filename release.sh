8b4f5484a1cd856aa8ef17f31e7d4f0c93e62340
JavaCommons Technologies <javacommons@gmail.com>
2025-04-20 08:08:12 +0900

    .

--- release.sh
+++ release.sh
@@ -1,11 +1,34 @@
 #! /usr/bin/bash
 set -uvx
 set -e
-pubspec-gen
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M"`
+version="${ts//.0/.}"
+
+echo $version
+
+pubspec-gen "$version"
 #dart pub get
+
+cat << EOS >> CHANGELOG.md
+
+## $version
+
+- pubspec.yaml was generated using pubspec-gen
+EOS
+
+dos2unix pubspec.yaml
+dos2unix CHANGELOG.md
+
 ./do-analyze.sh
 ./do-test.sh
-dart pub publish
+
+tag="$version"
 git add .
-git commit -m.
-git push
+git commit -m"$tag"
+git tag -a "$tag" -m"$tag"
+git push origin "$tag"
+git push origin HEAD:main
+git remote -v
+
+dart pub publish --force
